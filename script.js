8e13d79e9c4e10f62686a24143e408581897eddb
daferrom <daferrom@gmail.com>
2021-08-17 15:36:52 -0500

    JS in progress

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 /******* Cronometro *************/
+window.onload = (){
+    h= 1; 
+    m= 1; 
+    s= 1; 
+    mls= 1; 
+    timeStarted= 1;
+    time = document.getElementById("time");
+    bntStart = document.getElementById("btnStart")
+    btnStop = document.getElementById("btnStop")    
+    btnReset = document.getElementById("btnReset")
+    event();
+} 
+
+function event(){
+    bntStart.addEventListener("click", start)
+    btnStop.addEventListener("click", stop)
+    btnReset.addEventListener("click", reset)
+}
