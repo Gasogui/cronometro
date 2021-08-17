b0f0870f922a37ffe25065d60b6443ec4db95e77
Guillermo Garcia Soto <gasogui@gmail.com>
2021-08-17 16:31:56 -0500

    Modificación variable del root

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 /******* Cronometro *************/
 window.onload = (){
-    h= 1; 
-    m= 1; 
-    s= 1; 
-    mls= 1; 
-    timeStarted= 1;
+    h = 1;
+    m = 1;
+    s = 1;
+    mls = 1;
+    timeStarted = 1;
     time = document.getElementById("time");
     bntStart = document.getElementById("btnStart")
-    btnStop = document.getElementById("btnStop")    
+    btnStop = document.getElementById("btnStop")
     btnReset = document.getElementById("btnReset")
     event();
-} 
+}
 
-function event(){
+function event() {
     bntStart.addEventListener("click", start)
     btnStop.addEventListener("click", stop)
     btnReset.addEventListener("click", reset)
